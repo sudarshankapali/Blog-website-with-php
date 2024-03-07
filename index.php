ad8ae07197ba2ea27e4a57ab64b04ffb79fe8397
sudarshankapali <sudarshankapalisk@gmail.com>
2024-03-07 23:40:45 +0545

    setting up files

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta
+    name="description"
+    content="A blog website to express your thoughts and publish your passion your way!!!">
+    <title>Blog</title>
+    <link rel="stylesheet" href="./static/css/style.css">
+</head>
+<body>
+    <main>
+        <section class="header" title="navigation">
+            <h1>this is heading</h1>
+        </section>
+        <section class="hero" title="hero-section">
+            <h1>this is hero section</h1>
+        </section>
+        <section class="footer" title="footer">
+            <h1>this is footer section</h1>
+        </section>
+    </main>
+</body>
+</html>
